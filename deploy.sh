b85d4bbd128700daa5e5f39585bbee814e8694bc
Oleg Kipling <oleg.kipling@gmail.com>
2016-01-20 18:25:39 +0300

    travis fix

--- deploy.sh
+++ deploy.sh
@@ -1,43 +1,8 @@
-#!/bin/bash
-set -e # exit with nonzero exit code if anything fails
-
-# clear and re-create the out directory
+set -e
 cd ./build/
 git init
-
-# run our compile script, discussed above
-./compile.sh
-
-# go to the out directory and create a *new* Git repo
-cd out
-git init
-
-# inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
 git config user.email "oleg.kipling@gmail.com>"
-
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
 git add .
 git commit -m "Deploy to GitHub Pages"
-
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet 
-	"https://${GH_TOKEN}@${GH_REF}" 
-	master:gh-pages
-	> /dev/null 2>&1
-
-
-
-# Force push from the current repo's master branch to the remote
-# repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-
-# git push --force --quiet \
-#     git@github.com:Aidbox/Aidbox.github.io.git \
-#     master:master \
-#     > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
